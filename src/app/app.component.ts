a9ddd90abe3a7ced016e3aa6c21a7c7ea87ccac5
Amad Ul Hassan <ah@indigilabs.com>
2025-02-01 20:50:16 +0100

    showing actors in select
    showing actors ins select and implemented submit for finding path

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { GraphService } from './services/graph.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [FormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'ShortestPathToTomCruise';
+export class AppComponent implements OnInit {
+  actors: string[] = [];
+  selectedActor: string = '';
+  shortestPath: string = '';
+  constructor(private graphService: GraphService) {}
+  ngOnInit(): void {
+    this.actors = this.graphService.getActors();
+  }
+
+  findPath(): void {
+    if (this.selectedActor) {
+      this.shortestPath = this.graphService.findShortestPath(
+        this.selectedActor
+      );
+    }
+  }
 }
